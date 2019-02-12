dce7b5739e8713c3bf50f7ea414a923901be9cde
harshit.kumar <harshit.kumar@guavus.com>
2019-02-12 13:14:49 +0530

    test

--- scripts/test_rpm.sh
+++ scripts/test_rpm.sh
@@ -1,5 +1,6 @@
 EXECUTE_TEST=$1
 #pip install tox
 echo -e "# # # # # # # START : Testing RPM against Database # # # # # # #"
+pip install tox
 tox -e ${EXECUTE_TEST}
 echo -e "# # # # # # # END : Testing RPM against Database # # # # # # #"

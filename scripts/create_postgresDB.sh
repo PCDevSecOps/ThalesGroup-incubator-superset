b959742ac233c01c6d4392ad18d80922be4e0252
harshit.kumar <harshit.kumar@guavus.com>
2019-03-07 22:32:09 +0530

    feat(enable-CI): enabling unittest on CI
    UIC-835

--- scripts/create_postgresDB.sh
+++ scripts/create_postgresDB.sh
@@ -1,4 +1,4 @@
 echo -e "# # # # # # # START : Creating Postgres Database # # # # # ## #"
-psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'superset'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE superset"
+psql postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'superset'" | grep -q 1 || psql postgres -c "CREATE DATABASE superset"
 echo -e "# # # # # # # START : Creating PostGres User# # # # # # #"
-psql -U postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='postgresuser'" | grep -q 1 || psql -U postgres -c "CREATE USER postgresuser WITH PASSWORD 'pguserpassword';"
+psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='postgresuser'" | grep -q 1 || psql postgres -c "CREATE USER postgresuser WITH PASSWORD 'pguserpassword';"

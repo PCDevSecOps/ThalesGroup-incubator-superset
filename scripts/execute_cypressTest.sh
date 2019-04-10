bf00e6857d567e29370e2b03dd8ecd3c7f9969f7
harshit.kumar <harshit.kumar@guavus.com>
2019-04-10 09:18:42 +0530

    feat(enable-e2e): enabling e2e test using cypress
    UIC-424

--- scripts/execute_cypressTest.sh
+++ scripts/execute_cypressTest.sh
@@ -4,7 +4,7 @@ echo -e "# # # # # # # STARTING : Integration Test Exceution # # # # # # #"
 ls -a
 npm install cypress
 
-tox -e cypress-dashboard
-# tox -e cypress-explore
+#tox -e cypress-dashboard
+tox -e cypress-explore
 # tox -e cypress-sqllab
 echo -e "# # # # # # # COMPLETED : Integration Test Exceution # # # # # # #"

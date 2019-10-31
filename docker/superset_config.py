65890cd90d5bde875a51fec9dc9386ba781d0eb1
Subham Sarkar <subham.sarkar@guavus.com>
2019-10-31 16:26:25 +0530

    Expose variables for configuration changes

--- docker/superset_config.py
+++ docker/superset_config.py
@@ -56,6 +56,8 @@ WALKME_ENABLED = boolify(get_env_variable('WALKME_ENABLED'))
 # Help configuration
 HELP_ENABLED = boolify(get_env_variable('HELP_ENABLED'))
 
+# Log format and level configuration
+LOG_FORMAT = get_env_variable('LOG_FORMAT')
 LOG_LEVEL = boolify(get_env_variable('LOG_LEVEL'))
 
 #stale session timeout
@@ -150,10 +152,14 @@ TICKET_GENERATION_SYSTEM_ENDPOINT = get_env_variable('TICKET_GENERATION_SYSTEM_E
 TICKET_GENERATION_SYSTEM_API_KEY = get_env_variable('TICKET_GENERATION_SYSTEM_API_KEY','')
 TICKET_GENERATION_SYSTEM_USER = get_env_variable('TICKET_GENERATION_SYSTEM_USER','')
  
+# Set to False to disable all CSRF protection.
+WTF_CSRF_ENABLED = boolify('WTF_CSRF_ENABLED')
+# Max age in seconds for CSRF tokens. Default is 3600. If set to None, the CSRF token is valid for the life of the session.
+WTF_CSRF_TIME_LIMIT = get_env_variable('WTF_CSRF_TIME_LIMIT')
+
 # expose deployement var for WTF_CSRF_EXEMPT_LIST
 WTF_CSRF_EXEMPT_STR = get_env_variable('WTF_CSRF_EXEMPT_STR').strip()
 if WTF_CSRF_EXEMPT_STR:
     WTF_CSRF_EXEMPT_LIST = WTF_CSRF_EXEMPT_STR.split(",")
 
-ENABLE_CHUNK_ENCODING =  boolify(get_env_variable('ENABLE_CHUNK_ENCODING',"True"))  
-   
+ENABLE_CHUNK_ENCODING =  boolify(get_env_variable('ENABLE_CHUNK_ENCODING',"True"))

51343616e02aeb14706d8f2e0d5ecc62a8b04f85
Subham Sarkar <subham.sarkar@guavus.com>
2019-11-19 16:38:49 +0530

    Fix UTs for hive query partition related changes

--- tests/hivequery_tests.py
+++ tests/hivequery_tests.py
@@ -48,19 +48,19 @@ class HivePartitionQueryTestCase(unittest.TestCase):
         t_p_1 = {
             "year":"y"
         }
-        self.assertEqual(get_partitioned_query_format(t_p_1),"( y = %Y )")
+        self.assertEqual(get_partitioned_query_format(t_p_1),"( `y` = %Y )")
         t_p_2 = {
             "year":"y",
             "month":"m"
         }
-        self.assertEqual(get_partitioned_query_format(t_p_2),"( y = %Y AND m = %m )")
+        self.assertEqual(get_partitioned_query_format(t_p_2),"( `y` = %Y AND `m` = %m )")
 
         t_p_3 = {
             "year":"y",
             "month":"m",
             "day":"d",
         }
-        self.assertEqual(get_partitioned_query_format(t_p_3),"( y = %Y AND m = %m AND d = %d )")
+        self.assertEqual(get_partitioned_query_format(t_p_3),"( `y` = %Y AND `m` = %m AND `d` = %d )")
 
         t_p_4 = {
             "year":"y",
@@ -68,7 +68,7 @@ class HivePartitionQueryTestCase(unittest.TestCase):
             "day":"d",
             "hour":"hr"
         }
-        self.assertEqual(get_partitioned_query_format(t_p_4),"( y = %Y AND m = %m AND d = %d AND hr = %H )")
+        self.assertEqual(get_partitioned_query_format(t_p_4),"( `y` = %Y AND `m` = %m AND `d` = %d AND `hr` = %H )")
 
         t_p_5 = {
             "year":"y",
@@ -77,13 +77,13 @@ class HivePartitionQueryTestCase(unittest.TestCase):
             "hour":"hr",
             "minute":"min"
         }
-        self.assertEqual(get_partitioned_query_format(t_p_5),"( y = %Y AND m = %m AND d = %d AND hr = %H AND min = %M )")
+        self.assertEqual(get_partitioned_query_format(t_p_5),"( `y` = %Y AND `m` = %m AND `d` = %d AND `hr` = %H AND `min` = %M )")
         t_p_6 = {
             "month":"m",
             "day":"d",
             "hour":"hr",
         }
-        self.assertEqual(get_partitioned_query_format(t_p_6),"( m = %m AND d = %d AND hr = %H )")
+        self.assertEqual(get_partitioned_query_format(t_p_6),"( `m` = %m AND `d` = %d AND `hr` = %H )")
 
     def test_get_partitioned_whereclause(self):
         from_date_time_str = '2019-01-01 10:00:00'
@@ -97,18 +97,18 @@ class HivePartitionQueryTestCase(unittest.TestCase):
             "hour":"hr",
         }
         grain =  get_partitions_min_grain(t_p_5)
-        updated_sql = "( y = 2019 AND m = 01 AND d = 01 AND hr = 10 )" 
+        updated_sql = "( `y` = 2019 AND `m` = 01 AND `d` = 01 AND `hr` = 10 )" 
         self.assertEqual(get_partitioned_whereclause(from_date_time_obj,to_date_time_obj,grain,t_p_5),updated_sql)
 
     def test_replace_whereclause_in_org_sql(self):
         granularity = 'timestamp'
         sql = "WHERE `timestamp` >= '20190411000000' AND `timestamp` <= '20190412000000' AND `c_call_completed` >= 2"
-        where_clause = "( y = 2019 AND m = 01 AND d = 01 AND hr = 10 )" 
-        response = " WHERE ( y = 2019 AND m = 01 AND d = 01 AND hr = 10 ) AND  `timestamp` >= '20190411000000' AND `timestamp` <= '20190412000000' AND `c_call_completed` >= 2"
+        where_clause = "( `y` = 2019 AND `m` = 01 AND `d` = 01 AND `hr` = 10 )" 
+        response = " WHERE ( `y` = 2019 AND `m` = 01 AND `d` = 01 AND `hr` = 10 ) AND  `timestamp` >= '20190411000000' AND `timestamp` <= '20190412000000' AND `c_call_completed` >= 2"
         self.assertEqual(replace_whereclause_in_org_sql(granularity, sql, where_clause, False),response)
 
         sql = "WHERE `timestamp` >= 20190411000000 AND `timestamp` <= 20190412000000 AND `c_call_completed` >= 2"
-        response_1 = "WHERE ( y = 2019 AND m = 01 AND d = 01 AND hr = 10 ) \n AND `c_call_completed` >= 2"
+        response_1 = "WHERE ( `y` = 2019 AND `m` = 01 AND `d` = 01 AND `hr` = 10 ) \n AND `c_call_completed` >= 2"
         self.assertEqual(replace_whereclause_in_org_sql(granularity, sql, where_clause, True),response_1)
 
 
